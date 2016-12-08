7ff07bd46fefe27fd6a5487e311c683b7a229dc0
AngieMTZ53 <angielovesui95@gmail.com>
2016-12-08 05:31:10 -0600

    Primer Step de Eliminar y Modificar, ya paso :'c

--- RentaPeliculas/pruebas_funcionales/EliminarModificarPelicula/test/features/steps.py
+++ RentaPeliculas/pruebas_funcionales/EliminarModificarPelicula/test/features/steps.py
@@ -1,20 +1,26 @@
 # -*- coding: utf-8 -*-
-from lettuce import step
+from lettuce import step,world
+from selenium import webdriver
+import time
 
 @step(u'Dado que selecciono la pelicula "([^"]*)"')
 def dado_que_selecciono_la_pelicula_group1(step, pelicula):
-	titulo = world.driver.find_element_by_xpath('//table/tbody')
+	world.driver = webdriver.Chrome()
+	world.driver.get("http://192.168.33.10:8000/peliculas/lista/")
 
-    assert False, 'This step must be implemented'
+	titulo = world.driver.find_element_by_xpath('//table/tbody/tr[2]/td[2]')
+	assert True, pelicula == titulo.text
 @step(u'Cuando pesiono el boton "([^"]*)"')
-def cuando_pesiono_el_boton_group1(step, group1):
-    assert False, 'This step must be implemented'
+def cuando_pesiono_el_boton_group1(step, boton):
+	boton = world.driver.find_element_by_link_test("Borrar")
+	boton.click()
 @step(u'Entonces veo que la pelicula no esta en la lista de peliculas actualizada.')
 def entonces_veo_que_la_pelicula_no_esta_en_la_lista_de_peliculas_actualizada(step):
     assert False, 'This step must be implemented'
 @step(u'Cuando pesiono el boton "([^"]*)" y quito el estatus de "([^"]*)"')
-def cuando_pesiono_el_boton_group1_y_quito_el_estatus_de_group2(step, group1, group2):
-    assert False, 'This step must be implemented'
+def cuando_pesiono_el_boton_group1_y_quito_el_estatus_de_group2(step, nombre, button):
+	
+    
 @step(u'Entonces veo que la pelicula ya no esta rentada en la lista de peliculas.')
 def entonces_veo_que_la_pelicula_ya_no_esta_rentada_en_la_lista_de_peliculas(step):
     assert False, 'This step must be implemented'
